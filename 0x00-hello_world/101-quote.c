b9c44c4d609b7a4d9b7ce4b9cabae613bd677f1d
Iman Hamza <emyali697@gmail.com>
2022-12-08 17:36:54 -0800

    101-quote.c updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,7 @@
 
 int main(void)
 {
-
-	char s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	putchar(s);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
