e85f6cf1587425261a3f8066d310a6f73dfc42e1
Iman Hamza <emyali697@gmail.com>
2022-12-08 17:23:42 -0800

    101-quote.c created

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+
+	char s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	putchar(s);
+	return (1);
+
+}
