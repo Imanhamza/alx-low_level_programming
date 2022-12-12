96ba06e0cb972bb2482a6eded56649b782439dd5
Imanhamza <emyali697@gmail.com>
2022-12-12 07:12:17 +0200

    102-print_comb5.c created

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int first;
+	int second;
+
+	for (first = 0; first <= 9; first++)
+	{
+		for (second = first + 1; second <= 9; second++)
+		{
+			putchar(first);
+			putchar(second);
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
