c70113b3662dc2273e1a0be697d2cb4d4c28598e
Imanhamza <emyali697@gmail.com>
2022-12-12 06:07:31 +0200

    9-print_comb.c created

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		if (ch < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}
