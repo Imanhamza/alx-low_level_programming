30e9747038f2f625830680a1767c068bf7c4dcf7
Iman Hamza <emyali697@gmail.com>
2022-12-14 01:59:21 +0200

    main.h updated

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *  * File: main.h file
+ *   * Desc: contains The prototypes of all your functions
+ *    */
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
+#endif
