c62171d2f7019d08fb85ce3abc0e56660150dabb
Iman Hamza <emyali697@gmail.com>
2022-12-14 01:52:57 +0200

    2-print_alphabet_x10.c updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,14 @@
 
 void print_alphabet_x10(void)
 {
-	char ch;
+	char ch1;
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (ch = 'a'; ch <= 'z'; ch++)
+		for (ch1 = 'a'; ch1 <= 'z'; ch1++)
 		{
-			_putchar(ch);
+			_putchar(ch1);
 		}
 		_putchar('\n');
 	}
