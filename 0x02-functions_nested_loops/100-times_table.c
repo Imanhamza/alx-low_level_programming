97eb027fa0524be5c3b4c7be6a820355cb11c797
Imanhamza <71620286+Imanhamza@users.noreply.github.com>
2022-12-14 23:05:35 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,45 +10,40 @@ void print_times_table(int n)
 {
 if (n <= 15 && n > 0)
 	{
-		int i;
+		int i, j;
 		for (i = 0; i <= n; i++)
 		{
-			int j;
 			for (j = 0; j <= n; j++)
 			{
 				int multi = i * j;
 				if (j == 0)
 				{
 					_putchar('0' + multi);
-				}
-				else if (multi >= 10)
+				}else if (multi >= 10)
 				{
 					_putchar(' ');
 					_putchar('0' + (multi / 10));
 					_putchar('0' + (multi % 10));
-				}
-				else if (multi >= 100)
+				}else if (multi >= 100)
 				{
 					_putchar(' ');
 					_putchar('0' + (multi / 100));
 					_putchar('0' + ((multi / 10) % 10));
 					_putchar('0' + (multi % 10));
-				}
-				else
+				}else
 				{
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(multi + '0');
 				}
-				if (j != n)
+if (j != n)
 				{
 					_putchar(',');
 				}
 			}
 			_putchar('\n');
 		}
-	}
-	else
+	}else
 	{
 		_putchar('\n');
 	}
