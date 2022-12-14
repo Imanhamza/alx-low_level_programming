f989fe9cce6026aebd56f5be9e3554fc4c11222c
Iman Hamza <emyali697@gmail.com>
2022-12-14 04:50:03 +0200

    11-print_to_98.c created

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all number from n numer to 98
+ * @n: the start point
+ * Return: 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if ( n < 98)
+	{
+		int i;
+
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	 for (i = n; i >= 98; i--)
+	 {
+		 _putchar(i);
+		 _putchar(',');
+		 _putchar(' ');
+	 }
+	 _putchar('\n');
+}
