4f8c9c903cfd7273bb81d5f72831c213cb62292f
Iman Hamza <emyali697@gmail.com>
2022-12-14 03:27:52 +0200

    8-24_hours.c created

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every moment of the day of jack_bauer
+ * Return: 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int first;
+	int second;
+
+	for (first = 0; first <= 24; first++)
+	{
+		for (second = 0; second <= 60)
+		{
+			if (first <= 9 || second <= 9)
+			{
+				_putchar((first + '0') + ':' + (second + '0'))
+			}
+			_putchar(first + ':' + second)
+		}
+	}
+}
