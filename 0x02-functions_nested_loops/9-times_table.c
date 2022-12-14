ab2c81c337098e7271b775b3bc286518cab636d7
Iman Hamza <emyali697@gmail.com>
2022-12-14 04:21:10 +0200

    9-times_table.c created

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,25 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar(j + ',' + (j + j));
+			if ( j == 0)
+			{
+				_putchar((j*i) + '0');
+			} else
+			{
+				if ((j * i) < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((j*i) + '0');
+				}
+				else if ((j * i) >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(((j * i) / 10) + '0');
+					_putchar(((j * i) % 10) + '0');
+				}
 		}
 		_putchar('\n');
 	}
