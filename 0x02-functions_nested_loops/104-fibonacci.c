5e5c08be4fa76413630d4661ee351fb2ae42b3fb
Imanhamza <71620286+Imanhamza@users.noreply.github.com>
2022-12-14 22:43:57 +0200

    104-fibonacci.c created

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - prints first 98 fibonnacci numbers
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+	long int i, num1 = 1, num2 = 2, sum = 0;
+
+	for (i = 1; i < 98; i++)
+	{
+		printf("%ld, ", num1);
+		sum = num1 + num2;
+		num1 = num2;
+		num2 = sum;
+		if (i == 97)
+		{
+			printf("%ld\n", num1);
+		}
+	}
+}
