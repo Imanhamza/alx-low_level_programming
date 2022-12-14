0a5f2810215a7f6e4240dec7d13f7852a174ddfe
Iman Hamza <emyali697@gmail.com>
2022-12-14 05:01:36 +0200

    11-print_to_98.c updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,13 +13,11 @@ void print_to_98(int n)
 
 		for (i = n; i <= 98; i++)
 		{
-			_putchar(i + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d\n", i);
 		}
 	} else if ( n == 98)
 	{
-		_putchar(n + '0');
+		printf("%d\n", n);
 	}
 	else
 	{
@@ -27,10 +25,7 @@ void print_to_98(int n)
 
 		for (i = n; i >= 98; i--)
 		{
-			_putchar(i + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d\n", i);
 		}
 	}
-	_putchar('\n');
 }
