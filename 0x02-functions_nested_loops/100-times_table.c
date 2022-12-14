523cc2d5b90490a91fef8893518ff4d2bd7e4415
Iman Hamza <emyali697@gmail.com>
2022-12-14 20:17:16 +0200

    100-times_table.c updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,8 +10,7 @@ void print_times_table(int n)
 {
 	if (n >= 0 && n <= 15)
 	{
-		int i;
-		int j;
+		int i, j;
 
 		for (i = 0; i <= n; i++)
 		{
@@ -22,35 +21,36 @@ void print_times_table(int n)
 
 				if (j == 0)
 				{
-					_putchar('0' + multi);
+					_putchar(multi + '0');
 				}
-				else if (multi >= 10)
+				else if ( multi < 10 && j != 0)
+
 				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((multi % 10) + '0');
+				}
+				else if (multi >= 10 && multi < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar((multi / 10) + '0');
-					_putchar((multi % 10) + '0');
+					_putchar((multi % 10) _ '0');
 				}
 				else if (multi >= 100)
 				{
+					_putchar(',');
 					_putchar(' ');
 					_putchar((multi / 100) + '0');
 					_putchar(((multi / 10) % 10) + '0');
 					_putchar((multi % 10) + '0');
 				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(multi + '0');
-				}
-				if (j != n)
-				{
-					_putchar(',');
-				}
 			}
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-	_putchar('\n');
+
 }
