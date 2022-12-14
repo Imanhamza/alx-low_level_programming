5dcb1a878dd8ff90ebcf240d93a8d274db935532
Iman Hamza <emyali697@gmail.com>
2022-12-14 06:29:15 +0200

    7-print_last_digit.c updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,13 @@
 /**
  * print_last_digit - print the last digit of a certain number
  * @i: the number under test
- * Return : 0
+ * Return : 0 (Success)
  */
 
 int print_last_digit(int i)
 {
 	int ld = i % 10;
-	
+
 	if (ld < 0)
 	{
 		ld = -ld;
