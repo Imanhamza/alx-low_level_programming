c38c546324cef531bc2e062b1f7fe676a76e6879
Imanhamza <71620286+Imanhamza@users.noreply.github.com>
2022-12-14 23:09:17 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,43 +8,38 @@
 
 void print_times_table(int n)
 {
-if (n <= 15 && n > 0)
+	int i, j, out;
+
+	if (n >= 0 && n <= 15)
 	{
-		int i, j;
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar(48);
+			for (j = 1; j <= n; j++)
 			{
-				int multi = i * j;
-				if (j == 0)
+				outp = i * j;
+				_putchar(44);
+				_putchar(32);
+				if (out <= 9)
 				{
-					_putchar('0' + multi);
-				}else if (multi >= 10)
-				{
-					_putchar(' ');
-					_putchar('0' + (multi / 10));
-					_putchar('0' + (multi % 10));
-				}else if (multi >= 100)
-				{
-					_putchar(' ');
-					_putchar('0' + (multi / 100));
-					_putchar('0' + ((multi / 10) % 10));
-					_putchar('0' + (multi % 10));
-				}else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(multi + '0');
+					_putchar(32);
+					_putchar(32);
+					_putchar(out + 48);
 				}
-if (j != n)
+				else if (outp <= 99)
 				{
-					_putchar(',');
+					_putchar(32);
+					_putchar((out / 10) + 48);
+					_putchar((out % 10) + 48);
+				}
+				else
+				{
+					_putchar(((out / 100) % 10) + 48);
+					_putchar(((out / 10) % 10) + 48);
+					_putchar((out % 10) + 48);
 				}
 			}
 			_putchar('\n');
 		}
-	}else
-	{
-		_putchar('\n');
 	}
 }
