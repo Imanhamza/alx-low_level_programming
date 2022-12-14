2ca8f9d95b27ea9a00221eafa1a7a6efee893d4d
Iman Hamza <emyali697@gmail.com>
2022-12-14 10:40:58 +0200

    100-times_table.c created

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints n times table, starting with 0
+ * @n: number of tables
+ * Return: o (Success)
+ */
+
+void print_times_table(int n)
+{
+	if (n >= 0 && n <= 15)
+	{
+		int i;
+		int j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				int ij = i * j;
+
+				if (ij < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(ij + '0');
+				}
+				else if (ij >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((ij / 10) + '0');
+					_putchar((ij % 10) + '10');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+	break;
+}
