4fbf045194354396a7ad5e431ac2a933c0492d18
Iman Hamza <emyali697@gmail.com>
2022-12-16 04:13:26 +0200

    100-prime_factor.c created

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * Return: 0;
+ */
+
+int main(void)
+{
+	long n, i;
+
+	n = 612852475143;
+	for (i = 2; i < n; i++)
+	{
+		while (n % i = 0)
+		{
+			n = n / i;
+		}
+	}
+	printf("%l", n);
+}
