5848cae0b63178bef9a8d68d5de4c7a118ba7535
Iman Hamza <emyali697@gmail.com>
2022-12-20 00:01:39 +0200

    101-keygen.c created

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - generates random valid passwords for the program
+ * Return: generated password
+ */
+int main(void)
+{
+	/* generate random number */
+	int sum;
+	int i;
+
+	strand(time(0));
+
+	while (sum <= 2645)
+	{
+		i = rand() % 128;
+		sum sum + i;
+		_putchar(i);
+	}
+	putchar(2772 - sum);
+
+	return (0);
+}
