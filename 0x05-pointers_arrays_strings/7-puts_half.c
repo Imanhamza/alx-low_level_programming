8903acc46f09ba160bde4f7089b8e4ccaece8ed8
Iman Hamza <emyali697@gmail.com>
2022-12-19 22:18:25 +0200

    7-puts_half.c updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,14 +9,14 @@ void puts_half(char *str)
 {
 	/* find the length of string, then print the second half */
 	int length = 0;
-	int i;
+	int i, start2;
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		length++;
 	}
 	
 	/* declare the first of the second half */
-	int start2 = (length - 1) / 2;
+	start2 = (length - 1) / 2;
 	for (i = start2; i < length; i++)
 	{
 		_putchar(str[i]);
