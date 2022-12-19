6c6beb672d70964486416f495656bdb000e4f3f8
Iman Hamza <emyali697@gmail.com>
2022-12-19 23:27:57 +0200

    100-atoi.c created

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi -  convert a string to an integer
+ * @s: string
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	/* irritate the string to find out what in it */
+	int i;
+	int sign = 1;
+	unsigned int n = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (*s == '-')
+		{
+			i = -i;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			n = n * 10 + (*s - '0');
+		}
+		else if (n > 0)
+		{
+			break;
+		}
+		return (n * i);
+	}
+}
+
+
