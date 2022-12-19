d4cbbe3ac5ce85b788c3a5d36a4ed5f8e3244762
Iman Hamza <emyali697@gmail.com>
2022-12-19 23:42:31 +0200

    100-atoi.c updated

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,24 @@
 int _atoi(char *s)
 {
 	/* irritate the string to find out what in it */
-	int i;
 	int sign = 1;
 	unsigned int n = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	do
 	{
 		if (*s == '-')
 		{
 			sign = -sign;
 		}
-		else if (*s >= '0' && *s <= '9')
+		else if (*s >= '0' && *s <= 9)
 		{
-			n = n * 10 + (*s - '0');
+			n = n * sign;
+		}
+		else if (n > 0)
+		{
+			break;
 		}
 	}
+	while (*s++);
 	return (n * sign);
 }
-
-
