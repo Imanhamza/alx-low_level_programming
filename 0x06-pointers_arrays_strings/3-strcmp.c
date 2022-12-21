868c666f24ccedfdda130f81330d772529029faf
Iman Hamza <emyali697@gmail.com>
2022-12-21 22:02:52 +0200

    3-strcmp.c updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,13 +12,10 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int sum = 0;
 
-	for (i = 0; s1[i]; i++)
+	for (i = 0; s1[i] && s2[i]; i++)
 	{
-		for (i = 0; s2[i]; i++)
-		{
-			int diff = s1[i] - s2[i];
-			sum = sum + diff;
-		}
+		int diff = s1[i] - s2[i];
+		sum = sum + diff;
 	}
 	return (sum);
 }
