60941aa492ea92543043f244a5cbff9fb68a779b
Iman Hamza <emyali697@gmail.com>
2022-12-22 01:30:52 +0200

    7-leet.c updated

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,29 +8,20 @@
 
 char *leet(char *c)
 {
-	int i;
+	int i, j;
+	int upper[] = {65, 69, 79, 84, 76};
+	int lower[] = {97, 101, 111, 116, 108};
+	int integer[] = {52, 51, 55, 49};
 
 	for (i = 0; c[i] != '\0'; i++)
 	{
-		if (c[i] == 65 || c[i] == 97)
+		for (j = 0; j < 5; j++)
 		{
-			c[i] = 52;
-		}
-		else if (c[i] == 69 || c[i] == 101)
-		{
-			c[i] = 51;
-		}
-		else if (c[i] == 79 || c[i] == 111)
-		{
-			c[i] = 48;
-		}
-		else if (c[i] == 84 || c[i] == 116)
-		{
-			c[i] = 55;
-		}
-		else if (c[i] == 79 || c[i] == 108)
-		{
-			c[i] = 49;
+			if (c[i] == upper[j] || c[i] == lower[j])
+			{
+				c[i] = integer[j];
+				break;
+			}
 		}
 	}
 	return (c);
