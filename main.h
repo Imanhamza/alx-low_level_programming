a37730edc43e9862c1be0dace7112ae7ca02b47c
Iman Hamza <emyali697@gmail.com>
2023-01-02 17:21:51 +0200

    main.h created

---
+++ main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File: main.h file
+ * Desc: contains The prototypes of all Functions
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
