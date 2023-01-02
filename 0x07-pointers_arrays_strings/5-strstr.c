0d5e8392fe42ab4757ee78b4fc32250daadec447
Iman Hamza <emyali697@gmail.com>
2023-01-03 01:22:20 +0200

    5-strstr.c created

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strstr - a function that locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: A pointer to the beginning
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				return (needle);
+				break;
+			}
+		}
+	}
+	return (0);
+}
