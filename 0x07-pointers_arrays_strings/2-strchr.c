626afc0bdf78367b5cbb694faa2e50cf0bed3756
Iman Hamza <emyali697@gmail.com>
2023-01-02 20:36:10 +0200

    2-strchr.c updated

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,17 @@ char *_strchr(char *s, char c)
 {
 	/* irritate over the string to find the char */
 
-	while (*s)
+	while (*s != '\0')
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
 		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
 	}
 	return (0);
 }
