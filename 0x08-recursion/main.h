d232604df56d0f8fd972468e31b5f6b07c0608a4
Iman Hamza <emyali697@gmail.com>
2023-01-05 00:47:22 +0200

    main.h created

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *  * File: main.h file
+ *   * Desc: contains The prototypes of all Functions
+ *    */
+
+void _puts_recursion(char *s);
+
+#endif
