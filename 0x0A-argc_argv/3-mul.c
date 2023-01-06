b95c53b84028af3ab3b953405070ef42a5574d0e
Iman Hamza <emyali697@gmail.com>
2023-01-07 01:17:27 +0200

    3-mul.c created

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies two numbers.
+ * @argc: Number of attributes
+ * @arv: Array
+ * Return: Anumber
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+		return (0);
+	}
+}
