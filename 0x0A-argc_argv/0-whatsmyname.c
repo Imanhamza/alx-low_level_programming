edfa170132602586a1cd0d7aef62c2ea260585ca
Iman Hamza <emyali697@gmail.com>
2023-01-07 00:41:30 +0200

    0-whatsmyname.c created

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - a program that prints its name
+ * @argc: number of arguments
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
