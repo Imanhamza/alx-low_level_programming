c4e8b56eb5466d56db0a1325ae6edc44775b8c57
Iman Hamza <emyali697@gmail.com>
2023-01-06 23:30:17 +0200

    main.h updated

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #ifndef MAIN_H
 #define MAIN_H
 
 /**
- *  * File: main.h file
- *   * Desc: contains The prototypes of all your functions
- *    */
+ * File: main.h file
+ * Desc: contains The prototypes of all your functions
+ */
 
 int _putchar(char c);
 int _islower(int c);
