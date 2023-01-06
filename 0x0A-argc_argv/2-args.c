62906c3b7d46025ea81faea63cee0838f267a445
Iman Hamza <emyali697@gmail.com>
2023-01-07 01:05:36 +0200

    2-args.c updated

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,11 +7,11 @@
  * Return: 0
  */
 
-int main(int argc, char __attribute__((unused)) *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 1; i <= argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
