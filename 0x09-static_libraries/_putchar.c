4536d108be8f6feb6f59093be800bb94df74464b
Iman Hamza <emyali697@gmail.com>
2023-01-06 22:19:37 +0200

    files added

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch[] = "_putchar\n";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(ch[i]);
+	}
+	return (0);
+
+}
