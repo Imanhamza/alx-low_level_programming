4db4258486b9bec076959189688896374da891f1
Iman Hamza <emyali697@gmail.com>
2023-01-07 03:51:20 +0200

    4-add.c updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,13 +16,13 @@ int main(int argc, char *argv[])
 	int sum = 0,
 	    i;
 
-	if (argc == 0)
+	if (argc == 1)
 	{
 		printf("0\n");
 	}
 	else
 	{
-		for (i = 0; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
 			if (is_num(argv[i]) == 1)
 			{
