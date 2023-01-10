24e3feb8b6c88e4ed49ac19e0d16d132ec6520c4
Iman Hamza <emyali697@gmail.com>
2023-01-10 19:14:29 +0200

    2-str_concat.c updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,14 +24,13 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s2[i] != '\0'; i++)
 		size2++;
 
-	new = malloc((size1 * sizeof(*s1)) + (size2 * sizeof(*s2)));
+	new = malloc(size1 + size2 + 1);
 
 	for (i = 0; i < size1; i++)
 		new[i] = s1[i];
 
 	for (i = 0; i < size2; i++)
 		new[i + size1] = s2[i];
-	new[size1 + size2] = '\0';
 
 	if (new == NULL)
 		return (NULL);
