4282d09a28d7877c9e5e3aaa25f41b9768d7d571
Iman Hamza <emyali697@gmail.com>
2023-01-16 22:22:17 +0200

    2-print_dog.c updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,4 +16,6 @@ void print_dog(struct dog *d)
 		d->age == 0 ? printf("Age: (nil)\n") : printf("Age: %f\n", d->age);
 		d->owner == NULL ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
 	}
+	else
+		return;
 }
