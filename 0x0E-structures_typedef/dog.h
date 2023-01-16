cd36781b98779cd40923b0d30179ff562019e766
Iman Hamza <emyali697@gmail.com>
2023-01-16 21:18:40 +0200

    dog.h updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+
+/**
+ * struct dog - sturct DOG card
+ * @name: name of the dog
+ * @age: inger represents the age
+ * @owner: the name of the owner
+ */
+
 struct dog
 {
 	char *name;
