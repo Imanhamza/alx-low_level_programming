50de81a1f411f277f97400d6c4da8bd3268d6ec2
Iman Hamza <emyali697@gmail.com>
2023-01-27 00:24:56 +0200

    0-print_list.c updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,20 +9,21 @@
 
 size_t print_list(const list_t *h)
 {
+	const list_t *ap = h;
 	/* start countting the number of nodes */
 	size_t nodeCounter = 0;
 
-	while (h)
+	while (ap)
 	{
 		/* check if the str is NULL or not */
-		if (h->s == NULL)
+		if (ap->s == NULL)
 			printf("[%d] (nil)\n", 0);
 		else
-			printf("[%u] %s\n", h->len, h->s);
+			printf("[%d] %s\n", h->len, h->s);
 		/* move to the next node */
-		h = h->next;
+		ap = ap->next;
 		/* increase the counter */
-		nodeCounter += 1;
+		nodeCounter++;
 	}
 
 	return (nodeCounter);
