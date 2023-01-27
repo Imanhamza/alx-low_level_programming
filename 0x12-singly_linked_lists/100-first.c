80c3231ce0b7fcfd285dea0ca45da7769a0f2b85
Iman Hamza <emyali697@gmail.com>
2023-01-27 03:34:17 +0200

    100-first.c created

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * premain - function to return befor main
+ * Return: Nothing
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
