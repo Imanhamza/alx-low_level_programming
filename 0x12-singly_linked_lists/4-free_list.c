67748625abfac7fe5c5569e614faec7f5cde00f7
Iman Hamza <emyali697@gmail.com>
2023-01-27 02:58:44 +0200

    4-free_list.c created

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * free_list - a function that frees a list_t list.
+ * @head: A pointer to the head of the list
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	free(head->str);
+	free(head);
+}
