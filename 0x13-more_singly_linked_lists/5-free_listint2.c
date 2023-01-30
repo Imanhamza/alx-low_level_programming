c6f17a117a6065e8fa2709ec0da2ab66c9ffe6f2
Iman Hamza <emyali697@gmail.com>
2023-01-31 01:55:44 +0200

    5-free_listint2.c updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,12 +7,18 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = NULL;
+	listint_t *temp = NULL,
+		  *current;
 
-	while (head != NULL)
+	if (head != NULL)
 	{
-		temp = head;
-		head = head->next;
-		free(temp);
+		current = *head;
+
+		while ((temp = current) != NULL)
+		{
+			current = current->next;
+			free(temp);
+		}
+		*head = NULL;
 	}
 }
