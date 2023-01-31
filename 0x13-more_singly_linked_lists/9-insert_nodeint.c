610b77b559cfc86db8e493f8ee6aacbc29963ed1
Iman Hamza <emyali697@gmail.com>
2023-01-31 23:39:35 +0200

    9-insert_nodeint.c updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp->n = n;
 
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; i < idx - 1 && *head != NULL; i++)
 	{
 		current = current->next;
 		if (current == NULL)
