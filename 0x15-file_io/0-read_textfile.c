8e2ecbe78947e7c9e53bfd1115cfdc15d4f4ead3
Iman Hamza <emyali697@gmail.com>
2023-02-07 01:55:54 +0200

    fic betty style for 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	/* Allocate the memory for the buffer */
-	buffer = malloc(letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
