94c455582a03157213844157f0c2b564dd56a2e0
Iman Hamza <emyali697@gmail.com>
2023-02-07 02:16:36 +0200

    1-create_file.c updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content)
+	while (text_content != NULL)
 		strLen++;
 	/* set the values to be read and write */
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
