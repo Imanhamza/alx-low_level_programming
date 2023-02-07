abd507bbd52de51fa36506cae7928cc47b25ae47
Iman Hamza <emyali697@gmail.com>
2023-02-07 02:29:36 +0200

    fix betty style for 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,8 +2,8 @@
 
 /**
  * create_file - a function that creates a file.
- * filename: A char refer to the filename
- * text_content: The string
+ * @filename: A char refer to the filename
+ * @text_content: The string
  * Return: 1 on success, -1
  */
 int create_file(const char *filename, char *text_content)
