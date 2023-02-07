3dfaa202b17d4db9d7a1ca897281a582cf61c0a3
Iman Hamza <emyali697@gmail.com>
2023-02-07 02:40:44 +0200

    fix betty style for 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,27 +10,24 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	/* declare the variables for open, write and length */
 	int o,
-            w,
-            strLen = 0, /* counter for letters */
-            i;
+	    w,
+	    strLen = 0, /* counter for letters */
+	    i;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[strLen]; i++)
+			strLen++;
+	}
+	/* set the values to be read and write */
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, strLen);
+	/* check if the open and write doesnot fail */
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
+	return (1);
 
-        if (text_content != NULL)
-        {
-                for (i = 0; text_content[strLen]; i++)
-                        strLen++;
-        }
-        /* set the values to be read and write */
-        o = open(filename, O_WRONLY | O_APPEND);
-        w = write(o, text_content, strLen);
-
-        /* check if the open and write doesnot fail */
-        if (o == -1 || w == -1)
-                return (-1);
-
-        close(o);
-
-        return (1);
 }
