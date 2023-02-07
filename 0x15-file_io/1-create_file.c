a7585bf7c36044530b1fa8dfe38818267bd3f79a
Iman Hamza <emyali697@gmail.com>
2023-02-07 02:08:00 +0200

    1-create_file.c created

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file.
+ * filename: A char refer to the filename
+ * text_content: The string
+ * Return: 1 on success, -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	/* declare the variables for open, write and length */
+	int o,
+	    w,
+	    strLen = 0; /* counter for letters */
+
+	if (filename == NULL)
+		return (-1);
+
+	while (text_content)
+		strLen++;
+	/* set the values to be read and write */
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, sreLen);
+
+	/* check if the open and write doesnot fail */
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
