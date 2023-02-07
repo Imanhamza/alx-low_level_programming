5a663933264d66c88d05ae8ef73bd3857466f100
Iman Hamza <emyali697@gmail.com>
2023-02-07 03:07:50 +0200

    fix betty style for 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	if (r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return(98);
+		return (98);
 	}
 	w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (w == -1)
