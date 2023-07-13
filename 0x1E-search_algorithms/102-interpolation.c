d8e1295b72424b20b25207a93397e6b575282b45
Iman Hamza <iman.ali.hamza@gmail.com>
2023-07-12 17:56:18 -0700

    update 102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -20,7 +20,7 @@ int interpolation_search(int *array, size_t size, int value)
 	if (array == NULL || size == 0)
 		return (-1);
 
-	while (low <= high && value >= array[low] && value <= array[high])
+	for (; low <= high;)
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low])) *
 				(value - array[low]));
