2e0ef36cd98312fc0aeb3b39bfbcae602080ca33
Iman Hamza <iman.ali.hamza@gmail.com>
2023-07-12 19:33:04 -0700

    create 104-advanced_binary.c, update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,4 +13,8 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 
+/* Helpers */
+int advanced_binary_recursive(
+		int *array, size_t left, size_t right, int value);
+
 #endif
